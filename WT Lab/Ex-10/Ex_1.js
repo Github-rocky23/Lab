112c47df54a2dd79ed9a3ec60b001e8811f60514
Github-rocky23 <rocky26.09.23@gmail.com>
2024-11-11 09:55:14 +0530

    Add files via upload

---
+++ WT Lab/Ex-10/Ex_1.js
@@ -0,0 +1,9 @@
+var http=require('http');
+http.createServer(function(req,res))
+{
+    res.writeHead(200,{'Content-Type':'text/html'});
+    res.write('<b>my http module</b><br>');
+    res.write("The date and time are: "+new Data().toString()+"<br>");
+    res.write(req.url);
+    res.end();
+}.listen(4000);
